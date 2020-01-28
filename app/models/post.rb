a26f0afaccc26422aa904ba4530f9945aa5c1177
cejimenez0123 <christianjimenez0123@gmail.com>
2020-01-28 11:06:43 -0500

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,5 @@
 class Post < ActiveRecord::Base
+    validates :title, presence: true
+    validates :content, length: {minimum: 30}
+    validates :category, inclusion: {in: %w(Non-Fiction Fiction)}
 end
