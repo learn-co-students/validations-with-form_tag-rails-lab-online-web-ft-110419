a501074b5a2d6b1015d924895a336bac94b4d58c
Ivana <ivana_pile@yahoo.com>
2020-02-04 12:50:10 -0500

    Done.

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,4 +1,10 @@
 class AuthorsController < ApplicationController
+  
+  def index
+    @authors = Author.all
+  end
+
+
   def show
     @author = Author.find(params[:id])
   end
@@ -7,11 +13,31 @@ class AuthorsController < ApplicationController
     @author = Author.new
   end
 
-  def create
-    @author = Author.create(author_params)
-
-    redirect_to author_path(@author)
+  def edit
+    @author = Author.find(params[:id])
   end
+
+
+  def create
+    @author = Author.new(author_params)
+    if @author.valid?
+      @author.save
+      redirect_to author_path(@author)
+    else
+      render :new
+    end
+  end
+
+  def update
+    @author = author.find(params[:id])
+    if @author.update(author_params)
+      redirect_to author_path(@author)
+    else
+      render :edit
+    end
+
+  end
+
 
   private
 
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,9 @@
 class Post < ActiveRecord::Base
+    belongs_to :author
+    validates :title, presence: true
+    validates :content, length: {minimum: 100}
+    validates :category, inclusion: {in: ["Fiction", "Non-Fiction"]}
+#title is not blank
+#content is at least 100 characters long
+#category is either "Fiction" or "Non-Fiction"
 end
